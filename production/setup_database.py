7b3d6c641c2ead4bd160afd3ea6fb0cc46a20d0f
chrispsk <chrispsk@gmail.com>
2026-08-21 21:13:07 +0100

    Refactor database connection to use .env file path
    Updated database connection to load environment variables from a specific .env file path.

--- production/setup_database.py
+++ production/setup_database.py
@@ -3,17 +3,19 @@ import psycopg
 from pgvector.psycopg import register_vector
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 # -------------------- PostgreSQL connection --------------------
 
-load_dotenv()
+ENV_PATH = Path(__file__).resolve().parent / ".env"
+load_dotenv(ENV_PATH)
 
 connection = psycopg.connect(
-    dbname=os.getenv("POSTGRES_DB", "postgres"),
-    user=os.getenv("POSTGRES_USER", "postgres"),
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
     password=os.getenv("POSTGRES_PASSWORD"),
-    host=os.getenv("POSTGRES_HOST", "localhost"),
-    port=os.getenv("POSTGRES_PORT", "5432")
+    host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT")
 )
 cursor = connection.cursor()
 print("Connected to PostgreSQL.")
